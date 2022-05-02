73ec530ebe20c7c49ed34c73b0aabecb1a2e69c4
marcelo-ls-reis <100324942+marcelo-ls-reis@users.noreply.github.com>
2022-05-02 10:10:13 -0300

    Add files via upload
    aula webII 29/04/2022

---
+++ backend/src/api/controllers/indexControllers.js
@@ -0,0 +1,11 @@
+module.exports = {
+    index
+}
+
+function index(req, res) {
+    res.json('Rota Raiz Projeto Livros Encontrada!')
+    console.log('Rota Raiz Projeto Livros Encontrada!')
+
+}
+
+
---
+++ backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    host: 'localhost',
+    dialect : 'mysql',
+    username:'root',
+    password: "210396",
+    database: "webiiorm",
+    define:{
+        timestamps: true,
+        underscored: true,
+    }
+};
